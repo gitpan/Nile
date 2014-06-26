5bd9b44ea8e6b4a895175af54f6dcc1b8554870c
Ahmed Amin Elsheshtawy احمد امين الششتاوى جودة <mewsoft@cpan.org>
2014-06-26 02:00:15 +0000

    Import of MEWSOFT/Nile-0.0001 from CPAN.
    gitpan-cpan-distribution: Nile
    gitpan-cpan-version:      0.0001
    gitpan-cpan-path:         MEWSOFT/mewsoft/Nile-0.0001.tar.gz
    gitpan-cpan-author:       MEWSOFT
    gitpan-cpan-maturity:     released

---
+++ lib/Nile.pm
@@ -0,0 +1,31 @@
+#	Copyright Infomation
+#=========================================================#
+#	Module	:	Nile
+#	Author		:	Dr. Ahmed Amin Elsheshtawy, Ph.D.
+#	Website	:	http://www.mewsoft.com
+#	Email		:	mewsoft@cpan.org, support@mewsoft.com
+#	Copyrights (c) 2014-2015 Mewsoft Corp. All rights reserved.
+#=========================================================#
+
+=head1 NAME
+
+Nile - Visual Web App Framework Separating Code From Design Multi Lingual And Multi Theme.
+
+=head1 SYNOPSIS
+
+	use Nile;
+	my $app = Nile->new;
+	$app->init;
+	$app->run;
+
+=head1 DESCRIPTION
+
+Nile - Visual Web App Framework Separating Code From Design Multi Lingual And Multi Theme.
+
+=cut
+
+package Nile;
+
+our $VERSION = '0.0001';
+
+1;
