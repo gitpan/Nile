e0b7cce699e0d943e3199564fd6310abeee1eb3f
Ahmed Amin Elsheshtawy احمد امين الششتاوى جودة <mewsoft@cpan.org>
2014-07-10 04:33:01 +0000

    Import of MEWSOFT/Nile-0.12 from CPAN.
    gitpan-cpan-distribution: Nile
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         MEWSOFT/mewsoft/Nile-0.12.tar.gz
    gitpan-cpan-author:       MEWSOFT
    gitpan-cpan-maturity:     released

--- app/lib/Nile/Plugin/Home/Home.pm
+++ app/lib/Nile/Plugin/Home/Home.pm
@@ -8,6 +8,8 @@
 #=========================================================#
 package Nile::Plugin::Home::Home;
 
+our $VERSION = '0.11';
+
 use Nile::Base;
 #=========================================================#
 sub home  : GET Action {
@@ -49,5 +51,33 @@ sub home  : GET Action {
 
 }
 #=========================================================#
+
+=pod
+
+=head1 Bugs
+
+This project is available on github at L<https://github.com/mewsoft/Nile>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Nile>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/mewsoft/Nile>.
+
+=head1 AUTHOR
+
+Ahmed Amin Elsheshtawy,  احمد امين الششتاوى <mewsoft@cpan.org>
+Website: http://www.mewsoft.com
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014-2015 by Dr. Ahmed Amin Elsheshtawy احمد امين الششتاوى mewsoft@cpan.org, support@mewsoft.com,
+L<https://github.com/mewsoft/Nile>, L<http://www.mewsoft.com>
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
 
 1;
