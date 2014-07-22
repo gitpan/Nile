27a6f88a5d0eb3a5c9a5c2a72517a31e709196bb
Ahmed Amin Elsheshtawy احمد امين الششتاوى جودة <mewsoft@cpan.org>
2014-07-22 07:13:21 +0000

    Import of MEWSOFT/Nile-0.25 from CPAN.
    gitpan-cpan-distribution: Nile
    gitpan-cpan-version:      0.25
    gitpan-cpan-path:         MEWSOFT/Nile-0.25.tar.gz
    gitpan-cpan-author:       MEWSOFT
    gitpan-cpan-maturity:     released

---
+++ lib/Nile/Response.pm
@@ -0,0 +1,80 @@
+#	Copyright Infomation
+#=========================================================#
+#	Module	:	Nile::Response
+#	Author		:	Dr. Ahmed Amin Elsheshtawy, Ph.D.
+#	Website	:	https://github.com/mewsoft/Nile, http://www.mewsoft.com
+#	Email		:	mewsoft@cpan.org, support@mewsoft.com
+#	Copyrights (c) 2014-2015 Mewsoft Corp. All rights reserved.
+#=========================================================#
+package Nile::Response;
+
+our $VERSION = '0.23';
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Nile::Response -  The HTTP response manager.
+
+=head1 SYNOPSIS
+	
+		# get response instance
+		$response = $self->me->response;
+
+=head1 DESCRIPTION
+
+Nile::Response -  The HTTP response manager.
+
+=cut
+
+use Nile::Base;
+#=========================================================#
+#=========================================================#
+#=========================================================#
+#=========================================================#
+#=========================================================#
+#=========================================================#
+#=========================================================#
+#=========================================================#
+#=========================================================#
+sub object {
+	my $self = shift;
+	$self->me->object(__PACKAGE__, @_);
+}
+#=========================================================#
+
+=pod
+
+=head1 Bugs
+
+This project is available on github at L<https://github.com/mewsoft/Nile>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Nile>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/mewsoft/Nile>.
+
+=head1 SEE ALSO
+
+See L<Nile> for details about the complete framework.
+
+=head1 AUTHOR
+
+Ahmed Amin Elsheshtawy,  احمد امين الششتاوى <mewsoft@cpan.org>
+Website: http://www.mewsoft.com
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014-2015 by Dr. Ahmed Amin Elsheshtawy احمد امين الششتاوى mewsoft@cpan.org, support@mewsoft.com,
+L<https://github.com/mewsoft/Nile>, L<http://www.mewsoft.com>
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
